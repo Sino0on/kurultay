124b36ef298ca97fd8a32e58dc967c2e49bda9fb
baiel bekenov <baelbekbolotovb12356@gmail.com>
2023-02-11 00:39:20 +0600

    added multilanguage

--- server/urls.py
+++ server/urls.py
@@ -28,9 +28,9 @@ urlpatterns = [
     path('login/', loginpage, name='login'),
     path('resetPassword/', PasswordsChangeView.as_view(), name='resetPassword'),
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='resetPassword2.html'), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='reset_password_sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='reset_password_confirm.html'), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='reset_password_complete.html'), name='reset_password_complete'),
 
 ]
