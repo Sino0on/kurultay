fddb47d3b228d300b7613c3cc79321624b87d1d5
baiel bekenov <baelbekbolotovb12356@gmail.com>
2023-02-10 13:10:45 +0600

    totally modified

--- kurultay/urls.py
+++ kurultay/urls.py
@@ -7,6 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('server.urls')),
-    path('ba/', include('kurultai.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
